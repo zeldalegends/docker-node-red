e84c60cef4bdbea091f5e9b3c92791a5cb72120d
zeldalegends <73659648+zeldalegends@users.noreply.github.com>
2020-12-09 05:13:54 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Modbus TCP/IP and OPC UA sim server
-docker run --rm -it -p 50000:50000 -p 8080:8080 --name opcplc mcr.microsoft.com/iotedge/opc-plc:latest --pn=50000 --autoaccept --nospikes --nodips --nopostrend --nonegtrend --nodatavalues --sph --sn=25 --sr=10 --st=uint --fn=5 --fr=1 --ft=uint
-docker run --rm -p 5020:5020 oitc/modbus-server:latest
+docker run --rm -d -p 50000:50000 -p 8080:8080 mcr.microsoft.com/iotedge/opc-plc:latest --pn=50000 --autoaccept --nospikes --nodips --nopostrend --nonegtrend --nodatavalues --sph --sn=25 --sr=10 --st=uint --fn=5 --fr=1 --ft=uint
+docker run --rm -d -p 502:502 oitc/modbus-server:latest
 
 mkdir ./nodered/data
 sudo chown -R 1000:1000 .
