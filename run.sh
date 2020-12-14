5b258914bd8291f60d4c99e6400dcf2ca1f27248
zeldalegends <73659648+zeldalegends@users.noreply.github.com>
2020-12-14 21:43:53 +0100

    Update and rename test.sh to run.sh

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ mkdir ./nodered/data
 sudo chown -R 1000:1000 .
 
 #docker build -t node-red .
-docker-compose -f node-red.yml up
+docker-compose -f nodered.yml up
 
 echo "Grafana: http://127.0.0.1:3000 - admin/admin"
 
